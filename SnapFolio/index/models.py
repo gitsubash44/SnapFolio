14556690f706103925439af7c3ab916581c26275
gitsubash44 <dhamisubash866@gmail.com>
2025-09-15 21:02:07 +0545

    page update

--- SnapFolio/index/models.py
+++ SnapFolio/index/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class about(models.Model):
+    name = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    location = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='profile/')
+    badge_icon = models.CharField(max_length=50, blank=True)
+    description = models.TextField()
+    description_secondary = models.TextField(blank=True)
+    specialization = models.CharField(max_length=100)
+    experience_level = models.CharField(max_length=100)
+    education = models.CharField(max_length=200)
+    languages = models.CharField(max_length=200)
+    resume_link = models.URLField(blank=True)
+    talk_link = models.URLField(blank=True)
+
+    projects_completed = models.PositiveIntegerField(default=0)
+    years_experience = models.PositiveIntegerField(default=0)
+    client_satisfaction = models.PositiveIntegerField(default=0)  # Store as percentage
+
+    def __str__(self):
+        return self.name
--- SnapFolio/index/views.py
+++ SnapFolio/index/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import about
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+def about_view(request):
+    about_info = about.objects.first()
+    return render(request, 'index.html', {'about': about_info})
